458a1f9a39cc33d43690c86043811176b3400987
Carla <carla.holcomb25@gmail.com>
2019-08-21 21:00:27 -0400

    Add files via upload

---
+++ AddPlayer.java
@@ -0,0 +1,69 @@
+package exercise.murach.com.tictactoemod;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddPlayer extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Database mDatabase;
+    private Button btnAdd, btnViewData;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_player);
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnViewData = (Button) findViewById(R.id.btnView);
+        mDatabase = new Database(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddPlayer.this, Scoreboard.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabase.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package exercise.murach.com.tictactoemod;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void AddPlayer (View view){
+        startActivity(new Intent(MainActivity.this, AddPlayer.class));
+    }
+
+    public void Scoreboard (View view){
+        startActivity(new Intent(MainActivity.this, Scoreboard.class));
+    }
+
+    public void SelectPlayer1 (View view){
+        startActivity(new Intent(MainActivity.this, SelectPlayer.class));
+    }
+}
